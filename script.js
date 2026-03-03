18407a0844666d95c00dfedb1accec3602ff271f
pvardan053-ship-it <pvardan053@gmail.com>
2026-03-03 21:04:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
+const nightPrice = 100000;  
+const saunaPrice = 20000;   
 
-    const overnightInput = document.getElementById("overnightNights");
-    const saunaInput = document.getElementById("saunaDays");
-    const totalPriceEl = document.getElementById("totalPrice");
-    const form = document.getElementById("bookingForm");
+const bookingForm = document.getElementById("bookingForm");
+const totalPriceEl = document.getElementById("totalPrice");
 
-    function calculatePrice() {
-        let nights = Number(overnightInput.value) || 0;
-        let saunaDays = Number(saunaInput.value) || 0;
+const overnightNights = document.getElementById("overnightNights");
+const saunaDays = document.getElementById("saunaDays");
 
-        let total = 0;
+function updatePrice() {
+  let nights = Number(overnightNights.value) || 0;
+  let sauna = Number(saunaDays.value) || 0;
 
-        if (nights > 0) {
-            total = nights * 100000;
-        }
+  let total = nights * nightPrice + sauna * saunaPrice;
 
-        total += saunaDays * 20000;
+  totalPriceEl.textContent = total.toLocaleString();
+}
 
-        totalPriceEl.textContent = total.toLocaleString();
-    }
+overnightNights.addEventListener("input", updatePrice);
+saunaDays.addEventListener("input", updatePrice);
 
-    overnightInput.addEventListener("input", calculatePrice);
-    saunaInput.addEventListener("input", calculatePrice);
+bookingForm.addEventListener("submit", function(e){
+  e.preventDefault();
 
-    form.addEventListener("submit", function(e){
-        e.preventDefault();
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const people = document.getElementById("people").value;
+  const nights = Number(overnightNights.value) || 0;
+  const sauna = Number(saunaDays.value) || 0;
 
-        let name = document.getElementById("name").value;
-        let phone = document.getElementById("phone").value;
-        let people = document.getElementById("people").value;
-        let nights = overnightInput.value;
-        let sauna = saunaInput.value;
-        let total = totalPriceEl.textContent;
+  const total = nights * nightPrice + sauna * saunaPrice;
 
-        let message = `Բարև, ցանկանում եմ ամրագրել Siroon Tun։
+  let message = `Բարև, ցանկանում եմ ամրագրել Siroon Tun։
 Անուն՝ ${name}
 Հեռախոս՝ ${phone}
 Մարդկանց քանակ՝ ${people}
 Գիշերակացներ՝ ${nights}
 Շոքեբաղնիք օրեր՝ ${sauna}
-Ընդհանուր գումար՝ ${total} դրամ`;
+Ընդհանուր գումար՝ ${total.toLocaleString()} դրամ`;
 
-        const smsURL = "sms:094108617?body=" + encodeURIComponent(message);
-
-        window.location.href = smsURL;
-    });
-
+  const smsURL = "sms:098400070?body=" + encodeURIComponent(message);
+  window.location.href = smsURL;
 });
